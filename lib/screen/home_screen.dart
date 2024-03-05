8d206f5b6da92cb4fef1b4c3e7996378a69ea26f
few1123 <156067497+few1123@users.noreply.github.com>
2024-03-05 15:23:43 +0700

    Add files via upload

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,242 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:onboarding_screen/component/my_button.dart';
+import 'package:onboarding_screen/component/my_textfile.dart';
+import 'package:onboarding_screen/screen/sign_up_screen.dart';
+import 'package:onboarding_screen/component/my_iconbtn.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:onboarding_screen/screen/todolist_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  //Text Editing Controller
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  signInWithEmail() async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text,
+      );
+      _showMyDialog('Login successfully.');
+    } on FirebaseAuthException catch (e) {
+      print('Failed with error code: ${e.code}');
+      print(e.message);
+      if (e.code == 'invalid-email') {
+        _showMyDialog('No user found for that email.');
+      } else if (e.code == 'INVALID_LOGIN_CREDENTIALS') {
+        _showMyDialog('Wrong password provided for that user.');
+      }
+    }
+  }
+
+  void _showMyDialog(String txtMsg) async {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Expanded(
+          child: AlertDialog(
+            backgroundColor: Colors.amberAccent,
+            title: const Text('AlertDialog Title'),
+            content: Text(txtMsg),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.pop(context, 'Cancel'),
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed:() {
+                    Navigator.push(context,
+                     MaterialPageRoute(builder: (context) => const ToDoListScreen()
+                     ),
+                     );
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 20, 10, 20), //กำหนดระยะห่าง
+        child: Form(
+          child: Column(
+            children: [
+              const Spacer(),
+              Text(
+                "Hello, Ready To Get Started?",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.oswald(
+                  textStyle: Theme.of(context).textTheme.displayLarge,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                  fontStyle: FontStyle.normal,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text("Please sing-in with your email and password",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.lato(
+                    textStyle: Theme.of(context).textTheme.displayLarge,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w300,
+                    fontStyle: FontStyle.italic,
+                  )),
+              const SizedBox(
+                height: (30),
+              ),
+              MyTextField(
+                controller: emailController,
+                hintText: "Enter your Email",
+                obscureText: false,
+                labelText: "Email",
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              MyTextField(
+                controller: passwordController,
+                hintText: "Enter your password",
+                obscureText: true,
+                labelText: "Password",
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Forgot Password?',
+                        style: GoogleFonts.lato(
+                          textStyle: Theme.of(context).textTheme.displaySmall,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              MyButton(onTap: signInWithEmail, hintText: 'Sign in', ),
+              const SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: Text(
+                        'Or continue with',
+                        style: GoogleFonts.lato(
+                            textStyle: Theme.of(context).textTheme.displaySmall,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.blue),
+                      ),
+                    ),
+                    const Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  MyIconButton(imagePath: 'assets/images/google_icon.png'),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  MyIconButton(imagePath: 'assets/images/apple_iconpng.png'),
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Not a member?',
+                    style: GoogleFonts.lato(
+                      textStyle: Theme.of(context).textTheme.displayMedium,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.red,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 2,
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SignUpScreen(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      'Register Now',
+                      style: GoogleFonts.lato(
+                        textStyle: Theme.of(context).textTheme.displayMedium,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.orange,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const Spacer(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
